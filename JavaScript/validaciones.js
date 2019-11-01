480e37b9e6482901fd7ee0b16714482688f30f1a
Yordin Checo <Yordincheco@gmail.com>
2019-10-31 22:02:19 -0400

    Agreando cambios a los ejercicios

---
+++ JavaScript/validaciones.js
@@ -0,0 +1,40 @@
+function Evaluar() {
+  var Total = 0;
+  var evaluar = document.getElementById("p1");
+  var resultado = evaluar.options[evaluar.selectedIndex].value;
+
+  if (resultado == 1) {
+    Total = Total + 1;
+  }
+  evaluar = document.getElementById("p2");
+  resultado = evaluar.options[evaluar.selectedIndex].value;
+  if (resultado == 3) {
+    Total = Total + 1;
+  }
+  evaluar = document.getElementById("p3");
+  resultado = evaluar.options[evaluar.selectedIndex].value;
+
+  if (resultado == 3) {
+    Total = Total + 1;
+  }
+  evaluar = document.getElementById("p4");
+  resultado = evaluar.options[evaluar.selectedIndex].value;
+
+  if (resultado == 1) {
+    Total = Total + 1;
+  }
+  evaluar = document.getElementById("p5");
+  resultado = evaluar.options[evaluar.selectedIndex].value;
+
+  if (resultado == 2) {
+    Total = Total + 1;
+  }
+  evaluar = document.getElementById("p6");
+  resultado = evaluar.options[evaluar.selectedIndex].value;
+
+  if (resultado == 4) {
+    Total = Total + 1;
+  }
+
+  return Swal.fire("Has logrado", "Un total de: " + Total, "success");
+}
